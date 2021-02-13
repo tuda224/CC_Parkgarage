23cbdbe4958636a7ba6ff0ee7b794ce87b87b8d1
tuda224 <johann.tuder@gmail.com>
2021-02-13 14:53:28 +0100

    after level 4

--- CC_Parkgarage/Program.cs
+++ CC_Parkgarage/Program.cs
@@ -9,6 +9,7 @@ namespace CC_Parkgarage
     {
         private static Garage garage;
         private static LinkedList<int> carTracking;
+        private static Dictionary<int, int> carWeights;
 
         public static void Main(string[] args)
         {
@@ -25,21 +26,25 @@ namespace CC_Parkgarage
                  * negative number if parking lot is free
                  * entries are chronologically -> SortedList
                  * cars have to enter and leave once
-                 *
+                 * every car parks on lot with lowest number
+                 * every parking lot has specific price
+                 * on departing car pays no matter how long it was standing
 
                 */
                 var maxValues = GetMaximumSimultaneousCarsInGarage();
-                Console.WriteLine($"{maxValues.MaxInLot} {maxValues.MaxWaiting}");
+                //Console.WriteLine($"{maxValues.MaxInLot} {maxValues.MaxWaiting}");
+                Console.WriteLine(maxValues.Income);
             }
         }
 
         //  task: get maximum number of cars that have been at the same time in the garage
-        private static (int MaxInLot, int MaxWaiting) GetMaximumSimultaneousCarsInGarage()
+        private static (int MaxInLot, int MaxWaiting, int Income) GetMaximumSimultaneousCarsInGarage()
         {
             var node = carTracking.First;
             var maxInLot = 0;
-            var waitingQueue = new Queue<int>();
+            var waitingQueue = new Queue<Car>();
             var maxWaiting = 0;
+            var income = 0;
 
             while (node != null)
             {
@@ -50,18 +55,27 @@ namespace CC_Parkgarage
                     // if yes -> wait
                     if (garage.IsFull)
                     {
-                        waitingQueue.Enqueue(node.Value);
+                        waitingQueue.Enqueue(new Car
+                        {
+                            Number = node.Value,
+                            Weight = carWeights[node.Value]
+                        });
                     }
                     // if not -> reduce waiting list and add to lot
                     else
                     {
-                        garage.ParkCar(node.Value);
+                        garage.ParkCar(new Car
+                        {
+                            Number = node.Value,
+                            Weight = carWeights[node.Value]
+                        });
                     }
                 }
                 // car is departing
                 else
                 {
-                    garage.UnparkCar(node.Value);
+                    var lotInformation = garage.UnparkCar(node.Value);
+                    income += lotInformation.Price * (int)Math.Ceiling((double)lotInformation.Car.Weight / 100);
 
                     // check if someone is waiting
                     if (waitingQueue.Count > 0)
@@ -77,7 +91,7 @@ namespace CC_Parkgarage
                 node = node.Next;
             }
 
-            return (maxInLot, maxWaiting);
+            return (maxInLot, maxWaiting, income);
         }
 
         private static void ReadInput()
@@ -87,20 +101,43 @@ namespace CC_Parkgarage
 
             var lines = File.ReadLines("C:\\temp\\input.txt").ToArray();
             var garageSetup = lines[0].Split(' ');
-            var trackingList = lines[1].Split(' ');
+            var prices = lines[1].Split(' ');
+            var carweight = lines[2].Split(' ');
+            var trackingList = lines[3].Split(' ');
 
             garage = new Garage
             {
                 MaxNumberOfLots = int.Parse(garageSetup[0]),
                 TodaysNumberOfCars = int.Parse(garageSetup[1]),
-                Lots = new Dictionary<int, int>()
+                Lots = new Dictionary<int, LotInformation>()
             };
+
+            // init lots
+            for (int i = 1; i <= garage.MaxNumberOfLots; i++)
+            {
+                garage.Lots.Add(i, new LotInformation
+                {
+                    Price = int.Parse(prices[i - 1])
+                });
+            }
 
             carTracking = new LinkedList<int>();
             foreach (var car in trackingList)
             {
                 carTracking.AddLast(int.Parse(car));
             }
+
+            carWeights = new Dictionary<int, int>();
+            for (int i = 1; i <= carweight.Length; i++)
+            {
+                carWeights.Add(i, int.Parse(carweight[i - 1]));
+            }
+        }
+
+        public class Car
+        {
+            public int Number { get; set; }
+            public int Weight { get; set; }
         }
 
         public class Garage
@@ -110,20 +147,31 @@ namespace CC_Parkgarage
 
             public int TodaysNumberOfCars { get; set; }
 
-            public Dictionary<int, int> Lots { get; set; }
+            public Dictionary<int, LotInformation> Lots { get; set; }
 
-            public bool IsFull { get { return MaxNumberOfLots == Lots.Count; } }
+            public bool IsFull { get { return MaxNumberOfLots == Lots.Values.Where(l => l.Car != null).Count(); } }
 
-            public void ParkCar(int i)
+            public LotInformation ParkCar(Car car)
             {
-                Lots.Add(i, i);
+                var lotInformation = Lots.Values.Where(l => l.Car == null).First();
+                lotInformation.Car = car;
+                return lotInformation;
             }
 
-            public void UnparkCar(int i)
+            public (Car Car, int Price) UnparkCar(int i)
             {
                 i = i * -1;
-                Lots.Remove(i);
+                var lotInformation = Lots.Values.Where(l => l.Car != null && l.Car.Number == i).First();
+                var car = lotInformation.Car;
+                lotInformation.Car = null;
+                return (car, lotInformation.Price);
             }
+        }
+
+        public class LotInformation
+        {
+            public Car Car { get; set; }
+            public int Price { get; set; } = 0;
         }
     }
 }
