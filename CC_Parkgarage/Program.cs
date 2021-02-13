0e9bbee1c1e52392406b5ba59312b4106925a669
tuda224 <johann.tuder@gmail.com>
2021-02-13 10:55:34 +0100

    initial commit

---
+++ CC_Parkgarage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace CC_Parkgarage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                var input = ReadInput();
+                var splittedInput = input.Split(' ');
+                Console.WriteLine(input);
+            }
+        }
+
+        private static string ReadInput()
+        {
+            Console.WriteLine("Using input file: C:\\temp\\input.txt\nPress Return for run.");
+            Console.ReadLine();
+            var input = File.ReadAllText("C:\\temp\\input.txt");
+            return input;
+        }
+    }
+}
